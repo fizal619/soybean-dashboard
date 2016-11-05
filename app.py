9ca5d23709fc09f1f6ffac9fad81b69d045ba97a
Fizal Sarif <fizal.sarif@gmail.com>
2016-11-04 20:10:21 -0400

    changes so far

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
+# Other libs
+import 		json
+import 		twitter
+
 # Create an instance of flask, for some reason 
 # flask doesn't use app.listen() anymore? 
 # TODO: Read up on how to deploy flask app in 
 # late 2016
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__, static_url_path='/static')
 
 ###############################################
@@ -31,6 +35,53 @@ class TWEETS:
 # init 
 
 tweets = TWEETS()
+																							#
+																							#
+###############################################
+
+###############################################
+# Init Twitter API Wrapper -
+# Populates the queue also with soybean queries. 
+#
+#
+def loadCreds(fileName):	
+	with open(fileName) as json_data:
+		d = json.load(json_data)
+		return d
+
+creds = loadCreds('creds.json')
+
+api = twitter.Api(	consumer_key 				= creds['consumer_key'],
+                    consumer_secret			= creds['consumer_secret'],
+                    access_token_key		= creds['access_token_key'],
+                    access_token_secret	= creds['access_token_secret'])
+# print('########################################')
+# print('Testing creds for twitter:\n')
+# print(api.VerifyCredentials()) 
+# print('########################################')
+
+print('########################################')
+print('Loading the queue:\n')
+
+# print(results)
+print('########################################')
+
+																							#
+																							#
+###############################################
+
+
+
+###############################################
+# TEST ROUTE -
+# Used to test the data coming back from the 
+# twitter api in postman. 
+@app.route('/api/v1/test/search')
+def index():
+	results = api.GetSearch(
+    raw_query="q=%23soybean&count=20")
+	return results[0].AsJsonString()
+
 																							#
 																							#
 ###############################################
