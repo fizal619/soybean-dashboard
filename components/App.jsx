5c0c452a34435fb42000fba64e77d951b7dba7a3
Fizal Sarif <fizal.sarif@gmail.com>
2016-11-08 22:49:53 -0500

    finally understand redux and stuff

--- components/App.jsx
+++ components/App.jsx
@@ -3,7 +3,6 @@ import {Link, Route, Router, IndexRoute, browserHistory} from 'react-router'
 import Map from './screens/Map'
 
 const App=(props)=>{
-	counter++
 		return (
   		<Router key={Math.random()} history={browserHistory}>
 				<Route path="/" component={Map} position={props.position} />
